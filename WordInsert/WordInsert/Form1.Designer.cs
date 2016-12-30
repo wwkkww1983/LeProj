a8d27d175fd12a1ad3425b2397c0f1a1d5725eb6
zac <suoxd123@126.com>
2016-12-31 01:57:28 +0800

    word proj

---
+++ WordInsert/WordInsert/Form1.Designer.cs
@@ -0,0 +1,136 @@
+﻿namespace WordInsert
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnSource = new System.Windows.Forms.Button();
+            this.tbSource = new System.Windows.Forms.TextBox();
+            this.btnTarget = new System.Windows.Forms.Button();
+            this.tbTarget = new System.Windows.Forms.TextBox();
+            this.rtbResult = new System.Windows.Forms.RichTextBox();
+            this.lbResult = new System.Windows.Forms.Label();
+            this.btnExe = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnSource
+            // 
+            this.btnSource.Location = new System.Drawing.Point(24, 19);
+            this.btnSource.Name = "btnSource";
+            this.btnSource.Size = new System.Drawing.Size(111, 32);
+            this.btnSource.TabIndex = 0;
+            this.btnSource.Text = "初始文件夹";
+            this.btnSource.UseVisualStyleBackColor = true;
+            this.btnSource.Click += new System.EventHandler(this.btnSource_Click);
+            // 
+            // tbSource
+            // 
+            this.tbSource.Location = new System.Drawing.Point(136, 22);
+            this.tbSource.Name = "tbSource";
+            this.tbSource.Size = new System.Drawing.Size(706, 25);
+            this.tbSource.TabIndex = 1;
+            // 
+            // btnTarget
+            // 
+            this.btnTarget.Location = new System.Drawing.Point(24, 70);
+            this.btnTarget.Name = "btnTarget";
+            this.btnTarget.Size = new System.Drawing.Size(111, 32);
+            this.btnTarget.TabIndex = 0;
+            this.btnTarget.Text = "目标文件夹";
+            this.btnTarget.UseVisualStyleBackColor = true;
+            this.btnTarget.Click += new System.EventHandler(this.btnTarget_Click);
+            // 
+            // tbTarget
+            // 
+            this.tbTarget.Location = new System.Drawing.Point(136, 73);
+            this.tbTarget.Name = "tbTarget";
+            this.tbTarget.Size = new System.Drawing.Size(706, 25);
+            this.tbTarget.TabIndex = 1;
+            // 
+            // rtbResult
+            // 
+            this.rtbResult.Location = new System.Drawing.Point(24, 146);
+            this.rtbResult.Name = "rtbResult";
+            this.rtbResult.Size = new System.Drawing.Size(828, 449);
+            this.rtbResult.TabIndex = 2;
+            this.rtbResult.Text = "";
+            // 
+            // lbResult
+            // 
+            this.lbResult.AutoSize = true;
+            this.lbResult.Location = new System.Drawing.Point(775, 125);
+            this.lbResult.Name = "lbResult";
+            this.lbResult.Size = new System.Drawing.Size(67, 15);
+            this.lbResult.TabIndex = 3;
+            this.lbResult.Text = "执行结果";
+            // 
+            // btnExe
+            // 
+            this.btnExe.Location = new System.Drawing.Point(152, 104);
+            this.btnExe.Name = "btnExe";
+            this.btnExe.Size = new System.Drawing.Size(75, 36);
+            this.btnExe.TabIndex = 4;
+            this.btnExe.Text = "生成";
+            this.btnExe.UseVisualStyleBackColor = true;
+            this.btnExe.Click += new System.EventHandler(this.btnExe_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(864, 607);
+            this.Controls.Add(this.btnExe);
+            this.Controls.Add(this.lbResult);
+            this.Controls.Add(this.rtbResult);
+            this.Controls.Add(this.tbTarget);
+            this.Controls.Add(this.tbSource);
+            this.Controls.Add(this.btnTarget);
+            this.Controls.Add(this.btnSource);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(882, 652);
+            this.MinimumSize = new System.Drawing.Size(882, 652);
+            this.Name = "Form1";
+            this.Text = "Word字符插入";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSource;
+        private System.Windows.Forms.TextBox tbSource;
+        private System.Windows.Forms.Button btnTarget;
+        private System.Windows.Forms.TextBox tbTarget;
+        private System.Windows.Forms.RichTextBox rtbResult;
+        private System.Windows.Forms.Label lbResult;
+        private System.Windows.Forms.Button btnExe;
+    }
+}
+
---
+++ WordInsert/WordInsert/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using MSWord = Microsoft.Office.Interop.Word;
+using System.Reflection;
+
+namespace WordInsert
+{
+    public partial class Form1 : Form
+    {
+        FolderBrowserDialog dialog;
+        const string STRING_INSERT = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        static Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+            dialog = new FolderBrowserDialog();
+        }
+
+        private void btnSource_Click(object sender, EventArgs e)
+        {
+            openFolder("初始", tbSource);
+        }
+
+
+        private void btnTarget_Click(object sender, EventArgs e)
+        {
+            openFolder("目标", tbTarget);
+            while (tbTarget.Text.Trim().Equals(tbSource.Text.Trim()))
+            {
+                MessageBox.Show("文件夹不能相同");
+                openFolder("目标", tbTarget);
+            }
+        }
+
+        private void ComponentUseable(bool status)
+        {
+            btnSource.Enabled = status;
+            btnTarget.Enabled = status;
+            btnExe.Enabled = status;
+            tbSource.Enabled = status;
+            tbTarget.Enabled = status;
+            rtbResult.Enabled = status;
+        }
+
+        private void btnExe_Click(object sender, EventArgs e)
+        {
+            string pathSource = tbSource.Text.Trim(), pathTarget = tbTarget.Text.Trim();
+            if (pathTarget.Equals(string.Empty) || pathSource.Equals(string.Empty) || pathTarget.Equals(pathSource))
+            {
+                MessageBox.Show("请重新选择文件夹");
+                return;
+            }
+            //pathSource = "C:\\Users\\Administrator\\Desktop\\原来的";
+            //pathTarget = "C:\\Users\\Administrator\\Desktop\\改过的";
+            ComponentUseable(false);
+            TransformDocs(pathSource, pathTarget);
+            ComponentUseable(true);
+        }
+
+        /// <summary>
+        /// 读取文件夹路径
+        /// </summary>
+        /// <param name="folderName"></param>
+        /// <param name="tbBoard"></param>
+        private void openFolder(string folderName, TextBox tbBoard)
+        {
+            dialog.Description = "请选择" + folderName + "文档路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                string foldPath = dialog.SelectedPath;
+                tbBoard.Text = foldPath;
+            }
+        }
+
+        /// <summary>
+        /// 转换文件夹中的Word文件
+        /// </summary>
+        /// <param name="pathSource">源文件目录</param>
+        /// <param name="pathTarget">目标文件目录</param>
+        private void TransformDocs(string pathSource, string pathTarget)
+        {
+            int total = 0, fail = 0;
+            //路径
+            DirectoryInfo sourceFolder = new DirectoryInfo(pathSource);
+            FileInfo[] filesArray = sourceFolder.GetFiles();
+            object fileTarget = null;
+            //Word COM            
+            object Nothing = Missing.Value;                       //COM调用时用于占位
+            object format = MSWord.WdSaveFormat.wdFormatDocumentDefault; //Word文档的保存格式
+            MSWord.Application wordApp = new MSWord.Application();              //声明一个wordAPP对象
+            MSWord.Document worddoc = null;
+
+            foreach (FileInfo docItem in filesArray)
+            {
+                if ((docItem.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden || docItem.Extension != ".doc" && docItem.Extension != ".docx")
+                    continue;
+
+                try
+                {
+                    total++;
+                    lbResult.Text = total.ToString() ;
+                    worddoc = wordApp.Documents.Open(docItem.FullName, true);
+                    //核心工作：修改文档
+                    ChangeContents(worddoc);
+                    fileTarget = pathTarget + "\\" + docItem.Name;
+                    worddoc.SaveAs2(fileTarget);
+                }
+                catch (Exception ex)
+                {
+                    fail++;
+                    rtbResult.Text = docItem.Name + "\t" + ex.Message + ex.StackTrace +  "\r\n" + rtbResult.Text;
+                }
+                finally
+                {
+                    wordApp.ActiveDocument.Close(false);
+                    worddoc = null;
+                }
+            }
+            rtbResult.Text = "完成：\t成功：" + (total-fail).ToString () + "个；失败：" + fail.ToString () + "个；\r\n" + rtbResult.Text;
+            wordApp.Quit();
+        }
+
+        /// <summary>
+        /// 修改文档
+        /// </summary>
+        /// <param name="doc">文档内容</param>
+        private void ChangeContents(MSWord.Document doc)
+        {
+            int senTotal = 0, idx = 0;
+            MSWord.Range objRange = null;
+            string strTmp = string.Empty;
+            MSWord.Sentences content = null;
+
+            //doc.SetCompatibilityMode((int)MSWord.WdCompatibilityMode.wdWord2010);
+            content = doc.Sentences;
+            senTotal = content.Count;
+            for (int i = 1; i <= senTotal; i++)
+            {
+                objRange = content[i].Words.First;
+                idx = 1;
+                if (objRange.Text == "\r" || objRange.Next().Text == "\r" || objRange.Next().Next().Text == "\r")
+                    continue;//词语不足两个字则忽略
+                while (objRange.Text.IndexOf("\r") < 0)
+                {//插入字符
+                    objRange.InsertAfter(GetRandomChar());
+                    content[i].Words[++idx].Font.TextColor.ObjectThemeColor = MSWord.WdThemeColorIndex.wdThemeColorBackground1;
+                    content[i].Words[idx].Font.Fill.Transparency = 1F;
+                    content[i].Words[idx].Font.Spacing = -30;
+                    objRange = content[i].Words[++idx];
+                }
+            }
+
+            doc.Content.ParagraphFormat.AddSpaceBetweenFarEastAndAlpha = 0;
+            doc.Content.ParagraphFormat.AddSpaceBetweenFarEastAndDigit = 0;
+        }
+
+        /// <summary>
+        /// 生成随机字符
+        /// </summary>
+        /// <returns></returns>
+        private string GetRandomChar()
+        {
+            int idx = random.Next(1, STRING_INSERT.Length);
+            return STRING_INSERT[idx].ToString();
+        }
+    }
+}
