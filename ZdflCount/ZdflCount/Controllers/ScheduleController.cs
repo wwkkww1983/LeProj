ae458425db41406eb238b12fc7168d1cb1a62cc5
zac <suoxd123@126.com>
2017-10-20 15:48:26 +0800

    施工单管理

--- ZdflCount/ZdflCount/Controllers/ScheduleController.cs
+++ ZdflCount/ZdflCount/Controllers/ScheduleController.cs
@@ -22,7 +22,8 @@ namespace ZdflCount.Controllers
         public ActionResult Index(int id = 0)
         {
             var schedules = from item in db.Schedules
-                            where item.OrderId == id && item.Status != enumOrderStatus.Deleted
+                            where item.OrderId == id
+                            orderby item.Status ascending, item.DateLastUpdate descending
                             select item;
             if (schedules == null)
             {
@@ -48,6 +49,17 @@ namespace ZdflCount.Controllers
             return View(schedules);
         }
 
+        [HttpPost]
+        public ActionResult Assign(int id = 0)
+        {
+            Schedules schedules = db.Schedules.Find(id);
+            if (schedules == null)
+            {
+                return HttpNotFound();
+            }
+            return View("Details",  schedules );
+        }
+
         //
         // GET: /Schedule/Create/1
 
@@ -63,19 +75,23 @@ namespace ZdflCount.Controllers
         // POST: /Schedule/Create
 
         [HttpPost]
-        public ActionResult Create(Schedules schedules)
+        public ActionResult Create(Schedules schedules,int machine,int temporary=0)
         {
             if (ModelState.IsValid)
             {
                 Orders orderInfo = dbOrder.Orders.Find(schedules.OrderId);
-                if (orderInfo.ProductFreeCount < schedules.ProductCount)
+                if (temporary == 0 && orderInfo.ProductFreeCount < schedules.ProductCount)
                 {
                     this.modelSchOrder.Schedules = schedules;
                     this.modelSchOrder.Orders = orderInfo;
+                    this.modelSchOrder.SelectMachine(machine);
                     ViewData["error"] = "分派数量超过订单剩余总数";
                     return View(this.modelSchOrder);
                 }
 
+                schedules.MachineId = machine;
+                schedules.MachineName = this.modelSchOrder.MachineList.Find(item => int.Parse(item.Value) == machine).Text;
+                schedules.OrderNumber = orderInfo.OrderNumber;
                 schedules.DateCreate = DateTime.Now;
                 schedules.DateLastUpdate = DateTime.Now;
                 schedules.FinishCount = 0;
@@ -84,16 +100,20 @@ namespace ZdflCount.Controllers
                 schedules.LastUpdatePersonID = schedules.CreatorID;
                 schedules.LastUpdatePersonName = User.Identity.Name;
                 schedules.Number = string.Format("gd{0}{1}", schedules.CreatorID,DateTime.Now.ToString("yyyyMMddHHmmssffff"));
+                schedules.Status = temporary > 0 ? enumStatus.Temporary : enumStatus.Unhandle;
                 db.Schedules.Add(schedules);
                 db.SaveChanges();
 
-                orderInfo.ProductFreeCount -= schedules.ProductCount;
-                dbOrder.Entry(orderInfo).State = EntityState.Modified;
-                dbOrder.SaveChanges();
+                if (temporary == 0)
+                {
+                    orderInfo.ProductFreeCount -= schedules.ProductCount;
+                    dbOrder.Entry(orderInfo).State = EntityState.Modified;
+                    dbOrder.SaveChanges();
+                }
                 return RedirectToAction("Index", new { id = schedules.OrderId });
             }
 
-            return Content("输入数据无效");
+            return Content("输入创建施工单数据无效");
         }
 
         //
@@ -109,6 +129,7 @@ namespace ZdflCount.Controllers
 
             this.modelSchOrder.Schedules = schedules;
             this.modelSchOrder.GetOrderById(schedules.OrderId);
+            this.modelSchOrder.SelectMachine(schedules.MachineId);
 
             return View(this.modelSchOrder);
         }
@@ -117,29 +138,44 @@ namespace ZdflCount.Controllers
         // POST: /Schedule/Edit/5
 
         [HttpPost]
-        public ActionResult Edit(Schedules schedules)
+        public ActionResult Edit(Schedules schedules, int machine)
         {
             if (ModelState.IsValid)
             {
                 //db.Entry(schedules).State = EntityState.Modified;
                 db.Schedules.Attach(schedules);
                 var tempEntity = db.Entry(schedules);
-                tempEntity.Property(item => item.ProductCount).IsModified = true;
                 tempEntity.Property(item => item.FinishCount).IsModified = true;
                 tempEntity.Property(item => item.DetailInfo).IsModified = true;
                 tempEntity.Property(item => item.NoticeInfo).IsModified = true;
                 tempEntity.Property(item => item.LastUpdatePersonID).IsModified = true;
                 tempEntity.Property(item => item.LastUpdatePersonName).IsModified = true;
                 tempEntity.Property(item => item.DateLastUpdate).IsModified = true;
+                tempEntity.Property(item => item.MachineId).IsModified = true;
+                tempEntity.Property(item => item.MachineName).IsModified = true;
 
+                schedules.MachineId = machine;
+                schedules.MachineName = this.modelSchOrder.MachineList.Find(item => int.Parse(item.Value) == machine).Text;
                 schedules.LastUpdatePersonID = Convert.ToInt32(Session["UserID"]);
                 schedules.LastUpdatePersonName = User.Identity.Name;
                 schedules.DateLastUpdate = DateTime.Now;
 
+                if (schedules.ProductCount > 0)
+                {
+                    tempEntity.Property(item => item.ProductCount).IsModified = true;
+                    tempEntity.Property(item => item.Status).IsModified = true;
+                    schedules.Status = enumStatus.Unhandle;
+
+                    Orders orderInfo = dbOrder.Orders.Find(schedules.OrderId);
+                    orderInfo.ProductFreeCount -= schedules.ProductCount;
+                    dbOrder.Entry(orderInfo).State = EntityState.Modified;
+                    dbOrder.SaveChanges();
+                }
+
                 db.SaveChanges();
                 return RedirectToAction("Details", new { id = schedules.ID });
             }
-            return View(schedules);
+            return Content("输入修改施工单数据无效");;
         }
 
         //
@@ -166,11 +202,49 @@ namespace ZdflCount.Controllers
 
             db.Schedules.Attach(schedules);
             var tempEntity = db.Entry(schedules);
-            schedules.Status = enumOrderStatus.Deleted;
+            schedules.Status = enumStatus.Closed;
             schedules.LastUpdatePersonID = Convert.ToInt32(Session["UserID"]);
             schedules.LastUpdatePersonName = User.Identity.Name;
 
             db.SaveChanges();
+            return RedirectToAction("Index", new { id = schedules.OrderId });
+        }
+
+        //
+        // GET: /Schedule/Discard/5
+
+        public ActionResult Discard(int id = 0)
+        {
+            Schedules schedules = db.Schedules.Find(id);
+            if (schedules == null)
+            {
+                return HttpNotFound();
+            }
+            return View(schedules);
+        }
+
+        //
+        // POST: /Schedule/Delete/5
+
+        [HttpPost, ActionName("Discard")]
+        public ActionResult DiscardConfirmed(int id)
+        {
+            Schedules schedules = db.Schedules.Find(id);
+            //db.Schedules.Remove(schedules);
+
+            db.Schedules.Attach(schedules);
+            var tempEntity = db.Entry(schedules);
+            schedules.Status = enumStatus.Discard;
+            schedules.LastUpdatePersonID = Convert.ToInt32(Session["UserID"]);
+            schedules.LastUpdatePersonName = User.Identity.Name;
+
+            db.SaveChanges();
+
+            Orders orderInfo = dbOrder.Orders.Find(schedules.OrderId);
+            orderInfo.ProductFreeCount += schedules.ProductCount;
+            dbOrder.Entry(orderInfo).State = EntityState.Modified;
+            dbOrder.SaveChanges();
+
             return RedirectToAction("Index", new { id = schedules.OrderId });
         }
 
--- ZdflCount/ZdflCount/Models/ProductModels.cs
+++ ZdflCount/ZdflCount/Models/ProductModels.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Web;
 using System.Linq;
+using System.Web.Mvc;
 using System.Data.Entity;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -9,14 +10,8 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ZdflCount.Models
 {
-    public enum enumOrderStatus
+    public enum enumStatus
     {
-        /// <summary>
-        /// 已删除
-        /// </summary>
-        [Description("已删除")]
-        Deleted = -1,
-
         /// <summary>
         /// 未处理
         /// </summary>
@@ -30,16 +25,67 @@ namespace ZdflCount.Models
         Assigned,
 
         /// <summary>
-        /// 已中止
+        /// 工作中
         /// </summary>
-        [Description("已中止")]
-        Pause,
+        [Description("工作中")]
+        Working,
 
         /// <summary>
         /// 已完成
         /// </summary>
         [Description("已完成")]
-        Finished
+        Finished,
+
+        /// <summary>
+        /// 临时保存
+        /// </summary>
+        [Description("临时保存")]
+        Temporary,
+
+        /// <summary>
+        /// 已关闭
+        /// </summary>
+        [Description("已关闭")]
+        Closed,
+
+        /// <summary>
+        /// 已报废
+        /// </summary>
+        [Description("已报废")]
+        Discard
+    }
+
+    public enum enumMachineStatus
+    {
+        /// <summary>
+        /// 设备正常
+        /// </summary>
+        [Description("设备正常")]
+        Normal = 0x00,
+
+        /// <summary>
+        /// 设备故障
+        /// </summary>
+        [Description("设备故障")]
+        Invalid,
+
+        /// <summary>
+        /// 维修中
+        /// </summary>
+        [Description("维修中")]
+        Repair,
+
+        /// <summary>
+        /// 工作中
+        /// </summary>
+        [Description("工作中")]
+        Working,
+
+        /// <summary>
+        /// 设备报废
+        /// </summary>
+        [Description("设备报废")]
+        Discard
     }
 
     /// <summary>
@@ -61,7 +107,7 @@ namespace ZdflCount.Models
         /// <summary>
         /// 订单状态
         /// </summary>
-        public enumOrderStatus Status { get; set; }
+        public enumStatus Status { get; set; }
 
         /// <summary>
         /// 订单创建日期（订单自身属性）
@@ -129,11 +175,17 @@ namespace ZdflCount.Models
         /// 状态
         /// </summary>
         [DisplayName("工单状态")]
-        public enumOrderStatus Status { get; set; }
+        public enumStatus Status { get; set; }
 
         [StringLength(50)]
         [DisplayName("工单编号")]
         public string Number { get; set; }
+
+        [DisplayName("机台")]
+        public int MachineId { get; set; }
+
+        [DisplayName("机台")]
+        public string MachineName { get; set; }
 
         /// <summary>
         /// 订单编号
@@ -177,7 +229,7 @@ namespace ZdflCount.Models
         /// <summary>
         /// 商品总数量
         /// </summary>
-        [RegularExpression(@"^[1-9]\d*$", ErrorMessage = "仅可以输入正整数")]
+        [RegularExpression(@"^[0-9]\d*$", ErrorMessage = "仅可以输入正整数")]
         [DisplayName("需生产总数")]
         public int ProductCount { get; set; }
 
@@ -204,15 +256,71 @@ namespace ZdflCount.Models
         public DbSet<Schedules> Schedules { get; set; }
     }
 
+    public class Machines
+    {
+        [Key]
+        [DatabaseGeneratedAttribute(DatabaseGeneratedOption.Identity)]
+        [DisplayName("机台编号")]
+        public int ID { get; set; }
+
+        [DisplayName("机台名称")]
+        [StringLength(50)]
+        public string Name { get; set; }
+
+        [StringLength(50)]
+        [DisplayName("IP地址")]
+        public string IpAddress { get; set; }
+        
+        [DisplayName("Zigbee编号")]
+        public string ZigbeeNumber { get; set; }
+
+        [DisplayName("备注信息")]
+        public string RemarkInfo { get; set; }
+
+        
+        [DisplayName("设备状态")]
+        public enumMachineStatus Status { get; set; }
+
+    }
+
+    public class MachineDbContext : DbContext
+    {
+        public MachineDbContext()
+            : base("DefaultConnection")
+        {
+        }
+
+        public DbSet<Machines> Machines { get; set; }
+    }
+
     public class ScheduleOrder
     {
         public Schedules Schedules { get; set; }
         public Orders Orders { get; set; }
+        public List<SelectListItem> MachineList { get; private set; }
 
         private OrderDbContext dbOrder = new OrderDbContext();
 
         public ScheduleOrder()
-        {      
+        {
+            IEnumerable<Machines> allMachines = from item in new MachineDbContext().Machines
+                                                where item.Status == enumMachineStatus.Normal
+                                                select item;            
+            this.MachineList = new List<SelectListItem>(allMachines.Count<Machines>());
+            foreach (Machines item in allMachines)
+            {
+                this.MachineList.Add(new SelectListItem { Text = item.Name, Value = item.ID.ToString () });
+            }
+        }
+
+        public void SelectMachine(int machineId)
+        {
+            foreach (SelectListItem item in this.MachineList)
+            {
+                item.Selected = false;
+                if (int.Parse(item.Value) == machineId)
+                    item.Selected = true;
+            }
         }
 
         public void GetOrderById(int orderId)
